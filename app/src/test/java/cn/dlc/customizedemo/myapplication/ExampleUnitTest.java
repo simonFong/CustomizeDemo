943513a92a3c283848a4dfed19576e1234d0c31b
simonfong <fongzm_simon@163.com>
2019-06-28 13:37:19 +0800

    添加工具类

--- app/src/test/java/cn/dlc/customizedemo/myapplication/ExampleUnitTest.java
+++ app/src/test/java/cn/dlc/customizedemo/myapplication/ExampleUnitTest.java
@@ -3,6 +3,7 @@ package cn.dlc.customizedemo.myapplication;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.util.Calendar;
 
 import cn.dlc.customizedemo.myapplication.SerialPort.ByteUtil;
 
@@ -50,5 +51,50 @@ public class ExampleUnitTest {
     public static String binary(String s, int radix, int radix2) {
         return new BigInteger(s, radix).toString(radix2);// 这里的1代表正数
     }
+
+
+    @Test
+    public void main() {
+        boolean b = judgeIsIn(2, 5, "10:00", "11:30");
+        System.out.print("是否在范围里：" + b);
+    }
+
+    /**
+     * @param startweekday 1
+     * @param endweekday   7
+     * @param starttime    8:00
+     * @param endtime      10:00
+     */
+    public boolean judgeIsIn(int startweekday, int endweekday, String starttime, String endtime) {
+        //0.获取当前时间
+        Calendar instance = Calendar.getInstance();
+        int weekday = instance.get(Calendar.DAY_OF_WEEK) - 1 == 0 ? 7 : instance.get(Calendar.DAY_OF_WEEK) - 1;
+//        int weekday = instance.get(Calendar.DAY_OF_WEEK);
+        int hourOfDay = instance.get(Calendar.HOUR_OF_DAY);
+        int minuteOfDay = instance.get(Calendar.MINUTE);
+        //1.判断是否在规定的星期几以内
+        if (weekday < startweekday || weekday > endweekday) {
+            return false;
+        }
+        //2.判断是否在规定的小时以内
+        String[] startSplit = starttime.split(":");
+        int startHour = Integer.parseInt(startSplit[0]);
+        int startMinute = Integer.parseInt(startSplit[1]);
+        String[] endSplit = endtime.split(":");
+        int endHour = Integer.parseInt(endSplit[0]);
+        int endMinute = Integer.parseInt(endSplit[1]);
+        if (hourOfDay < startHour || hourOfDay > endHour) {
+            return false;
+        }
+        //3.判断是否在规定的分钟内
+        if (hourOfDay == startHour && minuteOfDay < startMinute) {
+            return false;
+        }
+        if (hourOfDay == endHour && minuteOfDay > endMinute) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
